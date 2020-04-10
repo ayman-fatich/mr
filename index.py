1b88693c506a16bf6375fe4111cef0baa1af9936
ayman-fatich <ayman.fatich.r@gmail.com>
2020-04-10 22:48:49 +0100

    i finished the backend

--- index.py
+++ index.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template, url_for, request, session, redirect, send_file
+from flask import Flask, render_template, url_for, request, session, redirect, send_file, send_from_directory, safe_join, abort
 import mysql.connector
 import datetime
 from io import BytesIO
+from werkzeug.security import generate_password_hash, check_password_hash
 import os
+
+
 x = datetime.datetime.now()
-UPLOAD_FOLDER = '/home/ayman/dev/mr/static/files'
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = '/home/ayman/dev/mr/static/files'
 
 
 app.secret_key = b'_5#y2L"F4Q8z@##HG]/'
@@ -24,10 +26,7 @@ crsr = mydb.cursor()
 @app.route('/')
 def home():
     if 'user_name' in  session:
-        session['user_name'] = session['user_name']
-        session['user_name'] = session['user_name']
-        session['field'] = request.form.getlist("field[]")
-        session['statue'] = session['statue']
+        
         return render_template('index.htm', uname=session['user_name'])
     else:
         return redirect(url_for('login'))
@@ -40,7 +39,7 @@ def login():
         user = crsr.fetchall()
         if user:
             user = user[0]
-            valid = valid_log(user[2], request.form['pwd'])
+            valid = check_password_hash(user[2], request.form['pwd'])
             if valid:
                 session['user_name'] = user[0]
                 session['statue'] = user[3]
@@ -61,11 +60,6 @@ def logout():
    session.clear()
    return redirect(url_for('login'))
 
-def valid_log(pwd, pwd1):
-    if pwd==pwd1 :
-        return True
-    else:
-        return  False
 
 @app.route('/singup', methods=['GET', 'POST'])
 def singup():
@@ -78,7 +72,8 @@ def singup():
             return render_template('login.htm', error=error)
         else:
             cmd = "INSERT INTO users (name, email, password, statue, field) VALUES (%s, %s, %s, %s, %s)"
-            nuser = (request.form['user_name'], request.form['email'], request.form['password'], request.form['statue'], request.form['field'])
+            password = generate_password_hash(request.form['password'])
+            nuser = (request.form['user_name'], request.form['email'], password, request.form['statue'], request.form['field'])
             crsr.execute(cmd, nuser)
             mydb.commit()
             session['user_name'] = request.form['user_name']
@@ -92,11 +87,17 @@ def singup():
 @app.route('/courses', methods=['GET', 'POST'])
 def courses():
     if request.method == 'POST':
-        subject = request.form["subject"]
-        crsr.execute("SELECT * FROM posts WHERE subject =  '%s'" %subject )
-        courses = crsr.fetchall()
+        if session['statue'] == 'student':
+            subject = request.form["subject"]
+            field = session['field']
+            crsr.execute("SELECT * FROM posts WHERE subject =  '{}' and field LIKE '%{}%' ".format(subject, field) )
+            courses = crsr.fetchall()
+        else:
+            subject = request.form["subject"]
+            crsr.execute("SELECT * FROM posts WHERE subject =  '{}'".format(subject))
+            courses = crsr.fetchall()
         
-        return render_template('courses.htm', courses = courses)
+        return render_template('courses.htm', courses=courses)
     return render_template('courses.htm')
 
 @app.route('/upload', methods=["GET", "POST"])
@@ -106,7 +107,7 @@ def upload():
         file = request.files['upFile']
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
 
-        dir = app.config['UPLOAD_FOLDER']+ file.filename
+        dir = file.filename
 
         cmd  = "INSERT INTO posts (title, author, file, date, subject, field ) VALUES (%s, %s, %s, %s, %s, %s)"
         post = (request.form["title"], session["user_name"], str(dir), x, str(request.form["subject"]), str(request.form.getlist("field[]")))
@@ -116,6 +117,15 @@ def upload():
         return redirect(url_for('upload'))
     else:
         return render_template('upload.htm')
+
+app.config["CLIENT_IMAGES"] = "/home/ayman/dev/mr/static/files"
+
+@app.route('/download/<filename>')
+def download(filename):
+    try:
+        return send_from_directory(app.config["CLIENT_IMAGES"], filename=filename, as_attachment=True)
+    except FileNotFoundError:
+        abort(404)
 
 if __name__ == '__main__':
     app.run(debug=True)    
