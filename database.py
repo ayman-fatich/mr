eacb8d1d099b0b407542b6d99e0c09a744dec561
ayman-fatich <ayman.fatich.r@gmail.com>
2020-04-10 10:05:22 +0100

    new bug

--- database.py
+++ database.py
@@ -3,12 +3,12 @@ import mysql.connector
 mydb = mysql.connector.connect(
 	host = "localhost",
 	user = "root",
-	passwd = "ayfaof26..",
-	database = "moulayrachid",
+	passwd = "AYfaof26..@",
+	database = "moulay_rachid",
 )
 
 crsr = mydb.cursor()
 
-crsr.execute("CREATE TABLE posts (title VARCHAR(50), author VARCHAR(50), file MEDIUMBLOB, date DATETIME, subject VARCHAR(100), field VARCHAR(50), post_id INTEGER AUTO_INCREMENT PRIMARY KEY)")
+crsr.execute("CREATE TABLE posts (title VARCHAR(50), author VARCHAR(50), file VARCHAR(150), date DATETIME, subject VARCHAR(100), field VARCHAR(50), post_id INTEGER AUTO_INCREMENT PRIMARY KEY)")
 mydb.commit()
 
--- index.py
+++ index.py
@@ -4,20 +4,20 @@ import datetime
 from io import BytesIO
 import os
 x = datetime.datetime.now()
-UPLOAD_FOLDER = 'Users/ayman/dev/mr/flask_backend/static'
+UPLOAD_FOLDER = '/home/ayman/dev/mr/static/files'
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.secret_key = b'_5#y2L"F4Q8z@##HG]/'
 
 
 mydb = mysql.connector.connect(
 	host = "localhost",
 	user = "root",
-	passwd = "ayfaof26..",
-	database = "moulayrachid",
+	passwd = "AYfaof26..@",
+	database = "moulay_rachid",
 )
 crsr = mydb.cursor()
 
@@ -26,7 +26,7 @@ def home():
     if 'user_name' in  session:
         session['user_name'] = session['user_name']
         session['user_name'] = session['user_name']
-        session['field'] = session['field']
+        session['field'] = request.form.getlist("field[]")
         session['statue'] = session['statue']
         return render_template('index.htm', uname=session['user_name'])
     else:
@@ -38,16 +38,21 @@ def login():
     if request.method == 'POST':
         crsr.execute("SELECT * FROM users WHERE email =  '%s'" %request.form['email'])
         user = crsr.fetchall()
-        user = user[0]
-        valid = valid_log(user[2], request.form['pwd'])
-        if valid:
-            session['user_name'] = user[0]
-            session['statue'] = user[3]
-            session['field'] = user[4]
-            return redirect(url_for('home'))
+        if user:
+            user = user[0]
+            valid = valid_log(user[2], request.form['pwd'])
+            if valid:
+                session['user_name'] = user[0]
+                session['statue'] = user[3]
+                session['field'] = user[4]
+                return redirect(url_for('home'))
+            else:
+                error = "Mot de pass incorrect"
+                return render_template('login.htm', error= error)
         else:
-            error = "Mot de pass incorrect"
+            error = "utilisateur introuvable"
             return render_template('login.htm', error= error)
+
     else:
         return render_template('login.htm', error= error)
 
@@ -69,7 +74,7 @@ def singup():
         crsr.execute("SELECT * FROM users WHERE email =  '%s'" %request.form['email'])
         user = crsr.fetchall()
         if user:
-            error = "l'utilisateur existe déjà"
+            error = "l'utilisateur existe deja"
             return render_template('login.htm', error=error)
         else:
             cmd = "INSERT INTO users (name, email, password, statue, field) VALUES (%s, %s, %s, %s, %s)"
@@ -97,10 +102,14 @@ def courses():
 @app.route('/upload', methods=["GET", "POST"])
 def upload():
     if request.method == 'POST':
+
         file = request.files['upFile']
-        file.save(os.path.join(app.config['UPLOAD_FOLDER']), file.filename)
-        cmd = sqlStuff = "INSERT INTO posts (title, author, file, date, subject, field ) VALUES (%s, %s, %s, %s, %s, %s)"
-        post = (request.form["title"], session['user_name'], file.read(), x, request.form["subject"], request.form.getlist("field[]"))
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+
+        dir = app.config['UPLOAD_FOLDER']+ file.filename
+
+        cmd  = "INSERT INTO posts (title, author, file, date, subject, field ) VALUES (%s, %s, %s, %s, %s, %s)"
+        post = (request.form["title"], session['user_name'], dir, x, request.form["subject"], request.form.getlist("field[]"))
         crsr.execute(cmd, post)
         mydb.commit()
 
